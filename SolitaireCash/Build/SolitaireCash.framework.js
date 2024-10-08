e057f28b4a98f4afd16ca28efa16fe9c9c8a030f
diemonic1 <diemonic1@gmail.com>
2024-10-08 14:07:22 +0300

    1

--- SolitaireCash/Build/SolitaireCash.framework.js
+++ SolitaireCash/Build/SolitaireCash.framework.js
@@ -1994,13 +1994,13 @@ var tempI64;
 // === Body ===
 
 var ASM_CONSTS = {
-  6912752: function() {return Module.webglContextAttributes.premultipliedAlpha;},  
- 6912813: function() {return Module.webglContextAttributes.preserveDrawingBuffer;},  
- 6912877: function() {return Module.webglContextAttributes.powerPreference;},  
- 6912935: function() {Module['emscripten_get_now_backup'] = performance.now;},  
- 6912990: function($0) {performance.now = function() { return $0; };},  
- 6913038: function($0) {performance.now = function() { return $0; };},  
- 6913086: function() {performance.now = Module['emscripten_get_now_backup'];}
+  6912736: function() {return Module.webglContextAttributes.premultipliedAlpha;},  
+ 6912797: function() {return Module.webglContextAttributes.preserveDrawingBuffer;},  
+ 6912861: function() {return Module.webglContextAttributes.powerPreference;},  
+ 6912919: function() {Module['emscripten_get_now_backup'] = performance.now;},  
+ 6912974: function($0) {performance.now = function() { return $0; };},  
+ 6913022: function($0) {performance.now = function() { return $0; };},  
+ 6913070: function() {performance.now = Module['emscripten_get_now_backup'];}
 };
 
 
